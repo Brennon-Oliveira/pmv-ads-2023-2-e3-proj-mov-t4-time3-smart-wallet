deaf91e844a4a2d02653411db646cb813be1c6a4
Brennon Gabriel de Oliveira <brennonoliveira20014@gmail.com>
2023-11-11 16:50:30 -0300

    Finalizando tela

--- src/pages/NovoLancamento.js
+++ src/pages/NovoLancamento.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Button, Touchable } from "react-native";
+import { StyleSheet, Text, View, Button, Touchable, Switch, TextInputComponent } from "react-native";
 import { TextInput } from "react-native-paper";
 import {Picker} from '@react-native-picker/picker';
 import { useState } from "react";
@@ -7,11 +7,10 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 import DateTimePicker from '@react-native-community/datetimepicker';
 import RNDateTimePicker from "@react-native-community/datetimepicker";
 
-
-
 const NovoLançamento = ({navigation}) => {
 
-    const [category, setCategory] = useState();
+    const [category, setCategory] = useState(false);
+    const [isEnabled, setIsEnabled] = useState(false);
 
     const goBack = ()=>{
         navigation.goBack();
@@ -48,11 +47,48 @@ const NovoLançamento = ({navigation}) => {
                       <Picker.Item label="Despesa" value="expenditure" />
                   </Picker>
                 </View>
-                <View>
-                  <View style={NewPostingStyles.form.group}>
+                <View style={NewPostingStyles.form.superGroup}>
+                  <View style={NewPostingStyles.form.subGroup}>
                     <Text style={NewPostingStyles.form.label}>Data de vencimento</Text>
                     <RNDateTimePicker value={new Date()}></RNDateTimePicker>
                   </View>
+                  <View style={NewPostingStyles.form.subGroup}>
+                    <Text style={NewPostingStyles.form.label}>Recorrente?</Text>
+                    <Switch
+                      onValueChange={()=>{setIsEnabled(!isEnabled)}}
+                      value={isEnabled}
+                    />
+                  </View>
+                </View>
+                <View style={NewPostingStyles.form.superGroup}>
+                  <View style={NewPostingStyles.form.subGroup}>
+                    <Text style={NewPostingStyles.form.label}>Valor</Text>
+                    <TextInput style={NewPostingStyles.form.input}/>
+                  </View>
+                  <View style={NewPostingStyles.form.subGroup}>
+                    <Text style={NewPostingStyles.form.label}>Status</Text>
+                    <Picker
+                      style={NewPostingStyles.form.picker}
+                      selectedValue={category}
+                      onValueChange={(itemValue, itemIndex) =>
+                        setCategory(itemValue)
+                      }>
+                      <Picker.Item label="Receita" value="Opção 1" />
+                      <Picker.Item label="Despesa" value="Opção 2" />
+                  </Picker>
+                  </View>
+                </View>
+                <View style={NewPostingStyles.form.group}>
+                  <Text style={NewPostingStyles.form.label}>Descrição</Text>
+                  <TextInput style={NewPostingStyles.form.input}/>
+                </View>
+                <View style={NewPostingStyles.form.buttonGroup}>
+                  <TouchableOpacity style={NewPostingStyles.form.buttonEdit}>
+                    <Text style={NewPostingStyles.form.buttonText}>Editar</Text>
+                    </TouchableOpacity>
+                  <TouchableOpacity style={NewPostingStyles.form.buttonSave}>
+                    <Text style={NewPostingStyles.form.buttonText}>Salvar</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         </View>
--- src/pages/styles/NovoLancamento.styles.js
+++ src/pages/styles/NovoLancamento.styles.js
@@ -10,11 +10,12 @@ const NewPostingStyles = {
     }),
     header: StyleSheet.create({
         container: {
-          flex: 1,
           display: "flex",
           alignItems: "center",
+          flexDirection: "row",
           justifyContent: "space-between",
           backgroundColor: "#fff",
+          height: 60
         },
         headerText: {
           fontSize: 20,
@@ -30,7 +31,47 @@ const NewPostingStyles = {
             display: "flex",
             margin: 20
         },
-        group: {},
+        superGroup: {
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+        },
+        subGroup: {
+            width:"50%"
+        },
+        group: {
+
+        },
+        buttonGroup: {
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            marginTop: 20
+        },
+        buttonEdit: {
+            backgroundColor: "#169CE3",
+            color: "#fff",
+            width: 50,
+            height: 30,
+            paddingHorizontal: 6,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 5
+        },
+        buttonSave: {
+            backgroundColor: "#416899",  
+            width: 50,
+            height: 30,
+            paddingHorizontal: 6,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 5
+        },
+        buttonText: {
+            color: "#fff",
+        },        
         label:{
             fontSize: 15,
             fontWeight: "bold",
@@ -42,7 +83,15 @@ const NewPostingStyles = {
             padding: 4,
             backgroundColor: "#fff",
             borderColor: "transparent"
+        },
+        input: {
+            width: "90%",
+            height: 40,
+            padding: 4,
+            backgroundColor: "#fff",
+            borderColor: "transparent"
         }
+        
     })
 }
 
